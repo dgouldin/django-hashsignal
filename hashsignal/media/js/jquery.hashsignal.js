5eb7e7039a91759a6ad191189b104b5814c197cf
David Gouldin <david@gould.in>
2010-10-16 20:15:55 -0600

    Removing Location dependency on jQuery, correcing Location.hash() to work like old $.hashsignal.hash() did.

--- hashsignal/media/js/jquery.hashsignal.js
+++ hashsignal/media/js/jquery.hashsignal.js
@@ -461,20 +461,38 @@ Requires
             protocol: '', // http:
             hostname: '', // www.google.com
             pathname: '', // /search
-            search: '',  // ?q=devmo
-            hash: ''  // #test
+            search: ''  // ?q=devmo
         };
         var that = this;
-        $.each(parts, function(k, v) {
-            that[k] = function(value) {
+        var partFunc = function(k) {
+            return function(value) {
                 if (value === undefined) {
                     return parts[k];
                 } else {
                     parts[k] = value;
+                    return value;
                 }
             };
-        });
+        }
+        for (var k in parts) {
+            if (parts.hasOwnProperty(k)) {
+                that[k] = partFunc(k);
+            }
+        }
 
+        parts.hash = '';
+        this.hash = function(value) { // #test
+            if (value === undefined) {
+                return parts.hash;
+            } else {
+                if (value.length === 0) {
+                    parts.hash = '';
+                } else {
+                    parts.hash = value[0] === '#' ? value : '#' + value;
+                }
+                return parts.hash;
+            }
+        };
         this.href = function(value) {  // http://www.google.com:80/search?q=devmo#test
             if (value === undefined) {
                 return this.protocol() + '//' + this.host() + this.pathname() + this.search() + this.hash();
